816f396d15e7a9f26c6686bda048c19ac6c409a3
Shabbir R Hassanally <shabbir@hassanally.net>
2022-04-07 00:09:11 +0100

    fixed swagger in prod

--- PrayerTimes.Api/Program.cs
+++ PrayerTimes.Api/Program.cs
@@ -63,13 +63,16 @@ builder.Services.AddSwaggerGen(options =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+//if (app.Environment.IsDevelopment())
+//{
+//    app.UseSwagger();
+//    app.UseSwaggerUI();
+//}
 
-app.UseHttpsRedirection();
+app.UseSwagger();
+app.UseSwaggerUI();
+
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
